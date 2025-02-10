c6a9b57703eb8423ae68f24c3b7c80b591766a9a
Wenhao Liu <wenliu@gmail.com>
2025-02-09 21:32:58 -0500

    feat: docker compose is working

--- server/src/modules/flask/flask.service.ts
+++ server/src/modules/flask/flask.service.ts
@@ -7,7 +7,7 @@ export class FlaskService {
   constructor(private readonly httpService: HttpService) {}
 
   async getHelloMessage(): Promise<any> {
-    const response = await lastValueFrom(this.httpService.get('http://backend-flask:5000/hello'));
-    return response;
+    const response = await lastValueFrom(this.httpService.get('http://backend-flask:5000/api/v1/hello'));
+    return response.data;
   }
 }
